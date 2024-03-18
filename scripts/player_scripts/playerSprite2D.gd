5a3049d638037e43454db5d51a129b1d788dd70b
AHuang8693 <Andy.Huang8693@gmail.com>
2024-03-17 17:07:43 -0700

    player sprite script - flips based on mouse position

--- scripts/player_scripts/playerSprite2D.gd
+++ scripts/player_scripts/playerSprite2D.gd
@@ -9,3 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_rotation = 0  
+	if get_global_mouse_position().x < get_parent().position.x:
+		flip_h = true
+	else:
+		flip_h = false
